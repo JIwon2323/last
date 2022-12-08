023cc219a602540760cfeabd62f3cadb56821fdf
LEE jiwon <you@example.com>
2022-12-08 11:53:34 +0900

    클릭영역 크게잡히는 버그 수정

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,16 @@
 const headerUserNameELement=document.querySelector('.user');
-const userNameELement=document.querySelector.apply('.user-name');
+
 const localUserName=localStorage.getItem('userName');
+const userNameElement=document.querySelector('#name');
+
 const setUserNameInnerHtml=(name)=>{
   headerUserNameELement.innerHTML='${name}<span>님</span>';
-  userNameELement.innerHTML ='${name}<span>님</span>';
+  userNameElement.textContent=name;
 };
 if (localUserName) {
   setUserNameInnerHtml(localUserName);
 }
-userNameELement.onclick=()=>{
+userNameElement.onclick=()=>{
   const userName = prompt('이름을 입력해주세요');
   localStorage.setItem('userName', userName);
   setUserNameInnerHtml(userName);
