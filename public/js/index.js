848b0ffd268e80789f9f830829564e94bad2b65c
JIwon2323 <115680981+JIwon2323@users.noreply.github.com>
2022-12-08 14:51:47 +0900

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,2 +1,18 @@
 const test=10;
 console.log(test);
+const headerUserNameELement=document.querySelector('.user');
+const localUserName=localStorage.getItem('userName');
+const userNameElement=document.querySelector('#name');
+
+const setUserNameInnerHtml=(name)=>{
+  headerUserNameELement.innerHTML='${name}<span>님</span>';
+  userNameElement.textContent=name;
+};
+if (localUserName) {
+  setUserNameInnerHtml(localUserName);
+}
+userNameElement.onclick=()=>{
+  const userName = prompt('이름을 입력해주세요');
+  localStorage.setItem('userName', userName);
+  setUserNameInnerHtml(userName);
+};
