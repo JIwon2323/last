4129e4903cc9f5bb985c19a2a60a62255390f326
JIwon2323 <115680981+JIwon2323@users.noreply.github.com>
2022-12-08 15:56:51 +0900

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,2 +1,68 @@
-const test=10;
-console.log(test);
+const reg2 = /[0-9]/;
+const reg3 = /^\w{5,12}@[a-z]{2,10}[\.][a-z]{2,3}[\.]?[a-z]{0,2}$/;
+const headerUserNameELement=document.querySelector('.user');
+const localUserName=localStorage.getItem('userName');
+const userNameElement=document.querySelector('#name');
+
+const setUserNameInnerHtml=(name)=>{
+  headerUserNameELement.innerHTML='${name}<span>님</span>';
+  userNameElement.textContent=name;
+};
+if (localUserName) {
+  setUserNameInnerHtml(localUserName);
+}
+userNameElement.onclick=()=>{
+  const userName = prompt('이름을 입력해주세요');
+  localStorage.setItem('userName', userName);
+  setUserNameInnerHtml(userName);
+};
+
+const localUserEmail=localStorage.getItem('userEmail');
+const userEmailElement=document.querySelector('#email');
+
+// eslint-disable-next-line no-unused-vars
+const setUserEmailInnerHtml=(email)=>{
+  userEmailElement.textContent=email;
+};
+if (localUserEmail) {
+  setUserEmailInnerHtml(localUserEmail);
+}
+
+userEmailElement.onclick=()=>{
+  const userEmail=prompt('메일을 입력해주세요');
+  localStorage.setItem('userEmail', userEmail);
+  setUserEmailInnerHtml(userEmail);
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    if (reg3.test(userEmail)) {
+      break;
+    }
+    alert('메일형식이 잘못되었습니다.');
+    // eslint-disable-next-line no-const-assign
+    userEmail=prompt('메일주소는');
+  }
+};
+const localUserId=localStorage.getItem('UserId');
+const UserIdElement=document.querySelector('#StudentID');
+
+const setUserIdInnerHtml=(StudentID)=>{
+  UserIdElement.textContent=StudentID;
+};
+if (localUserId) {
+  setUserIdInnerHtml(localUserId);
+}
+UserIdElement.onclick=()=>{
+  const UserId=prompt('학번을 입력해주세요');
+  localUserId.setItem('UserId', UserId);
+  setUserIdInnerHtml(UserId);
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    if (reg2.test(UserId)) {
+      break;
+    }
+    alert('학번형식이 잘못되었습니다.');
+    // eslint-disable-next-line no-const-assign
+    UserId=prompt('학번은');
+  }
+}
+;
